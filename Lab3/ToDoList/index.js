09bb6f438a9a47d38daa25ba51516391cf7a4c15
urluvcupid <el.talgatow@gmail.com>
2025-02-08 00:24:21 +0500

    lab3

---
+++ Lab3/ToDoList/index.js
@@ -0,0 +1,43 @@
+const inputBox = document.getElementById("inputTask")
+const listTask = document.getElementById("listInsertion")
+const deleteAllBtn = document.getElementById("deleteAllBtn");
+
+function addTask(){
+    if(inputBox.value === ""){
+        alert("write something!")
+    }
+    else{
+        li = document.createElement("li")
+        li.innerHTML = `
+                <input type = "checkbox">
+                <span class = "task-text">${inputBox.value}</span>
+                <button class= "delete_btn">🗑</button>`
+                listTask.appendChild(li);
+    }
+    inputBox.value="";
+    saveData();
+}
+
+listTask.addEventListener("click", function(e){
+    if(e.target.tagName === "INPUT"){
+        e.target.nextElementSibling.classList.toggle("completed");
+        saveData();
+    }
+    else if(e.target.tagName === "BUTTON"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+
+function saveData(){
+    localStorage.setItem("data", listTask.innerHTML)
+}
+function showTask(){
+    listTask.innerHTML = localStorage.getItem("data") || "";
+}
+showTask();
+
+
+// deleteAllBtn.addEventListener("click", function() {
+//     listTask.innerHTML = ""; 
+// });
