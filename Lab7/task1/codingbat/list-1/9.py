0ac8154a5420eeb2482cf6475210b58970471b3e
urluvcupid <el.talgatow@gmail.com>
2025-03-23 19:05:17 +0500

    Lab7

---
+++ Lab7/task1/codingbat/list-1/9.py
@@ -0,0 +1,7 @@
+
+def make_pi():
+    a = []
+    a.append(3)
+    a.append(1)
+    a.append(4)
+    return a
---
+++ Lab7/task1/codingbat/list-2/1.py
@@ -0,0 +1,6 @@
+
+def even(a):
+    cnt=0
+    for i in a:
+        if i%2==0:cnt+=1
+    return cnt
