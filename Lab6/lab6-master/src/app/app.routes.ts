e4b6eacb792210a1bb52a1e63e29541bbc29b5d3
urluvcupid <el.talgatow@gmail.com>
2025-03-23 18:23:53 +0500

    some changes

--- Lab6/lab6-master/src/app/app.routes.ts
+++ Lab6/lab6-master/src/app/app.routes.ts
@@ -4,8 +4,10 @@ import { AlbumComponent } from './album/album.component';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
 import { PhotosComponent } from './photos/photos.component';
 import { AppComponent } from './app.component';  
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
     { path: 'about', component: AboutComponent },
     { path: 'albums', component: AlbumComponent },
     { path: 'albums/:id', component: AlbumDetailComponent },
